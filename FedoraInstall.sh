5f54f6c2cec2b0dacfab3faac691906888b45d19
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2022-05-25 11:26:52 +0100

    Add files via upload

---
+++ FedoraInstall.sh
@@ -0,0 +1,93 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+if test -f "./NVIDIA-Linux-x86_64-510.73.05.run"
+then
+./NVIDIA-Linux-x86_64-510.73.05.run
+systemctl set-default graphical.target
+reboot
+else
+dnf update -y
+
+# Install KDE Packages
+dnf install \
+  @"base-x" \
+  @"Common NetworkManager Submodules" \
+  @"Fonts" \
+  @"Hardware Support" \
+  @"Virtualization" \ 
+  bluedevil \
+  breeze-gtk \
+  breeze-icon-theme \
+  cagibi \
+  colord-kde \
+  cups-pk-helper \
+  dolphin \
+  glibc-all-langpacks \
+  gnome-keyring-pam \
+  kcm_systemd \
+  kde-gtk-config \
+  kde-partitionmanager \
+  kde-print-manager \
+  kde-settings-pulseaudio \
+  kde-style-breeze \
+  kdegraphics-thumbnailers \
+  kdeplasma-addons \
+  kdialog \
+  kdnssd \
+  kf5-akonadi-server \
+  kf5-akonadi-server-mysql \
+  kf5-baloo-file \
+  kf5-kipi-plugins \
+  khotkeys \
+  kmenuedit \
+  konsole5 \
+  kscreen \
+  kscreenlocker \
+  ksshaskpass \
+  ksysguard \
+  kwalletmanager5 \
+  kwebkitpart \
+  kwin \
+  NetworkManager-config-connectivity-fedora \
+  pam-kwallet \
+  phonon-qt5-backend-gstreamer \
+  pinentry-qt \
+  plasma-breeze \
+  plasma-desktop \
+  plasma-desktop-doc \
+  plasma-drkonqi \
+  plasma-nm \
+  plasma-nm-l2tp \
+  plasma-nm-openconnect \
+  plasma-nm-openswan \
+  plasma-nm-openvpn \
+  plasma-nm-pptp \
+  plasma-nm-vpnc \
+  plasma-pa \
+  plasma-user-manager \
+  plasma-workspace \
+  plasma-workspace-geolocation \
+  polkit-kde \
+  qt5-qtbase-gui \
+  qt5-qtdeclarative \
+  sddm \
+  sddm-breeze \
+  sddm-kcm \
+  setroubleshoot \
+  sni-qt \
+  xorg-x11-drv-libinput -y
+
+wget https://uk.download.nvidia.com/XFree86/Linux-x86_64/510.73.05/NVIDIA-Linux-x86_64-510.73.05.run
+
+chmod +x ./NVIDIA-Linux-x86_64-510.73.05.run    
+
+reboot
+fi
+
+
+
